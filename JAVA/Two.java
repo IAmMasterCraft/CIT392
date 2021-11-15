ecbd3e98589d215bcc9f7476a124eb88b5dfd20e
Boluwaji Akinsefunmi <bolu.akinsefunmi@gmail.com>
2021-11-15 01:48:02 +0100

    new java solution

---
+++ JAVA/Two.java
@@ -0,0 +1,9 @@
+public class Two {
+    public static void main(String args[]) {
+        int result = 0;
+        for (int index = 1; index <= 100; index++) {
+            result += index;
+        }
+        System.out.println(result);
+    }
+}
